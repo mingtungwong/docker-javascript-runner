809993340614cf6a7109bea2b7627aaf6a09d7d5
Ming Tung Wong <mingtungwong@outlook.com>
2018-01-25 17:26:40 -0500

    set up server

---
+++ docker.js
@@ -0,0 +1,30 @@
+const Docker = require('dockerode');
+
+const streamBuffers = require('stream-buffers');
+const Bluebird = require('bluebird');
+
+function createDocker () {
+    this.docker = new Docker();
+}
+
+createDocker.prototype.runCommand = function (userCode, testCode, scenario) {
+
+   	let userCodeEdited = userCode.replace(/\"\'\"/gm, '\"\\\'\"').replace(/\'\"\'/gm, '\"\\\\\"\"').replace(/([^\\])'/gm, (match, p1) => p1 + '"');
+    userCodeEdited = ' \"use strict\"; var assert = require("assert");' + userCodeEdited + testCode;
+
+    const runUserCodeCommand = `touch test.js && echo $'${userCodeEdited}' > test.js && babel test.js --out-file test-compiled.js && mocha -R spec test-compiled.js && exit`;
+    const stdoutStream = new streamBuffers.WritableStreamBuffer();
+    const finishedPromise = new Bluebird((resolve, reject) => {
+        this.docker.run('mocha-chai-node', ['bash', '-c', runUserCodeCommand], stdoutStream, function(err, data, container) {
+            if (err) return reject(err);
+            const results = stdoutStream.getContentsAsString('utf8');
+            container.remove(function (error, response) {
+                if (error) console.log(error);
+            });
+            resolve(results.replace(/\[\d+m/gi, ""));
+        });
+    });
+    return finishedPromise;
+};
+
+module.exports = createDocker;
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 3000;
+const docker = require('./docker');
+
+app.listen(port, () => console.log(`I'm listening on port ${port}`));
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/code', (req, res, next) => {
+    const { code, tests } = req.body;
+    docker.runCommand(code, tests).then(results => res.send(results));
+})
